6dd18044b0b166a3099e0f0a89fa992cbbc1e58d
Bayu Putra L <putralaksmanabayu@gmail.com>
2025-02-18 12:09:06 +0700

    Learn Route

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,66 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'welcome';
 });
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/about', function () {
+    return 'NIM : 2341720240' . '<br>' . 'Nama : Bayu Putra Laksmana';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+});
+
+Route::get('articles/{id}', function ($id) {
+    return 'Halaman artikel dengan ID ' . $id;
+});
+
+Route::get('/user/{name?}', function ($name = null) {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/user/profile', function () {
+    //
+})->name('profile');
+
+Route::middleware(['first', 'second'])->group(function () {
+    Route::get('/', function () {
+        // Uses first & second middleware...
+    });
+
+    Route::get('/user/profile', function () {
+        // Uses first & second middleware...
+    });
+});
+
+Route::domain('{account}.example.com')->group(function () {
+    Route::get('user/{id}', function ($account, $id) {
+        //
+    });
+});
+
+//Redirect Route
+//Redirect ini akan sering digunakan pada kasus kasus CRUD atau kasus lain yang membutuhkan redirect ke halaman tertentu.
+Route::redirect('/here', '/there');
+
+//view Route
+//View Route ini akan sering digunakan pada kasus kasus CRUD atau kasus lain yang membutuhkan view tertentu.
+Route::view('/welcome', 'welcome');
+Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
