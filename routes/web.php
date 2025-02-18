9b597c6785b9488bc38aa1db1414d99312d03f7a
Bayu Putra L <putralaksmanabayu@gmail.com>
2025-02-18 14:16:58 +0700

    descriptoin

--- routes/web.php
+++ routes/web.php
@@ -73,6 +73,38 @@ Route::domain('{account}.example.com')->group(function () {
     });
 });
 
+//Route Group and Prefix
+//Route group ini akan sering digunakan pada kasus kasus CRUD atau kasus lain yang membutuhkan group tertentu.
+Route::middleware(['first', 'second'])->group(function () {
+    Route::get('/', function () {
+        // Uses first & second middleware...
+
+    });
+
+    Route::get('/user/profile', function () {
+        // Uses first & second middleware...
+    });
+});
+
+Route::domain('{account}.example.com')->group(function () {
+    Route::get('user/{id}', function ($account, $id) {
+        //
+    });
+});
+Route::middleware('auth')->group(function () {
+    Route::get('/user', [UserController::class, 'index']);
+    Route::get('/post', [PostController::class, 'index']);
+    Route::get('/event', [EventController::class, 'index']);
+});
+
+//Route prefix
+//Route prefix ini akan sering digunakan pada kasus kasus CRUD atau kasus lain yang membutuhkan prefix tertentu.
+Route::prefix('admin')->group(function () {
+    Route::get('/user', [UserController::class, 'index']);
+    Route::get('/post', [PostController::class, 'index']);
+    Route::get('/event', [EventController::class, 'index']);
+});
+
 //Redirect Route
 //Redirect ini akan sering digunakan pada kasus kasus CRUD atau kasus lain yang membutuhkan redirect ke halaman tertentu.
 Route::redirect('/here', '/there');
